88bfee5cc80bff5508c5c68bbfde252db6b8cb93
joc <jc_perezvaldez@hotmail.com>
2013-09-24 15:39:12 -0500

    7 secciones

--- controlador.rb
+++ controlador.rb
@@ -18,6 +18,22 @@ get '/dos' do
   erb :dos
 end
 
+get '/codigos-de-barras' do
+  erb :codigos_de_barras
+end
 
+get '/impresoras-de-codigos-de-barras' do
+  erb :impresoras_de_codigos_de_barras
+end
 
+get '/impresora-de-etiquetas-itt4100' do
+  erb :im_eq_itt4100
+end
 
+get '/nosotros' do
+  erb :nosotros
+end
+
+get '/contacto' do
+  erb :contacto
+end
